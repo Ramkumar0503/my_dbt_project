4819797377cc822c6b1b4275ec7544bbc4bac2db
Ramkumar0503 <ramkumarayyadurai05@gmail.com>
2025-07-23 17:13:22 +0530

    Delete models/informatica_transform.sql

--- models/informatica_transform.sql
+++
@@ -1,9 +0,0 @@
-WITH customer_orders AS (
-    SELECT
-        customer_id,
-        COUNT(*) AS total_orders,
-        SUM(order_amount) AS total_amount
-    FROM DBT_DB.DBT_SCHEMA.ORDERS
-    GROUP BY customer_id
-)
-SELECT * FROM customer_orders
