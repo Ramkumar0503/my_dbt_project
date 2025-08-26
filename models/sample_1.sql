281728efc737026bc179681e76b3504d5aaadf07
Ramkumar0503 <ramkumarayyadurai05@gmail.com>
2025-08-26 18:30:27 +0530

    Add files via upload

---
+++ models/sample_1.sql
@@ -0,0 +1,24 @@
+{{
+	config(
+		materialized='incremental',
+		alias='EXP_G20_SWP_VALUATIONSOut',
+		schema='',
+		pre_hook ="",
+		post_hook ="",
+		incremental_strategy='append'
+	)
+}}
+Select
+	'LEI_' || LPAD('abc', 5, '0') as O_LEGAL_ENTITY_IDENTIFIER,
+    LENGTH(PARTY_CODE_DISPLAY) as CODE_DISPLAY,
+    PARTY_CODE_PREFIX_ALT_DISPLAY,
+    UTI,
+    o_UPI,
+    NPV,
+    LENGTH(CURRENCY_CODE) as CURRENCY_CODE,
+    VALUATION_METHOD,
+    VALUATION_TIMESTAMP,
+    ACTION_TYPE,
+    Proprietary_UTI_indicator
+from
+	EXP_G20_SWP_VALUATIONSOut
