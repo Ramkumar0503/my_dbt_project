c65f46fb26e7de6ae500db6872d26d1170a50236
Ramkumar0503 <ramkumarayyadurai05@gmail.com>
2025-09-11 10:47:53 +0530

    Update sample_2.sql

--- models/sample_2.sql
+++ models/sample_2.sql
@@ -1,25 +1,20 @@
-{{
-	config(
-		materialized='incremental',
-		alias='EXP_G20_SWP_VALUATIONSOUT_2',
-		schema='',
-		pre_hook ="",
-		post_hook ="",
-		incremental_strategy='append'
-	)
-}}
-Select
-	'LEI_' || LPAD('abc', 5, '0') as O_LEGAL_ENTITY_IDENTIFIER,
+{{ config(
+    materialized='incremental',
+    alias='EXP_G20_SWP_VALUATIONSOUT_2',
+    schema='',
+    incremental_strategy='append'
+) }}
+select
+    'LEI_' || LPAD('abc', 5, '0') as O_LEGAL_ENTITY_IDENTIFIER,
     PARTY_CODE_DISPLAY,
     PARTY_CODE_PREFIX_ALT_DISPLAY,
     LENGTH(UTI) AS UTI_LENGTH,
-	UTI,
+    UTI,
     o_UPI,
     NPV,
     LENGTH(CURRENCY_CODE) as CURRENCY_CODE,
     VALUATION_METHOD,
-    VALUATION_TIMESTAMP,
+    "VALUATION_TIMESTAMP",
     ACTION_TYPE,
     Proprietary_UTI_indicator
-from
-	EXP_G20_SWP_VALUATIONSOut
+from {{ ref('EXP_G20_SWP_VALUATIONSOUT_1') }}
