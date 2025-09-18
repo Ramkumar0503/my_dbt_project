835a42b2c30195090f2c081ffc1997bf2af72ed0
Ramkumar0503 <ramkumarayyadurai05@gmail.com>
2025-09-18 17:44:39 +0530

    Update sample_1.sql

--- models/sample_1.sql
+++ models/sample_1.sql
@@ -1,26 +1,36 @@
 {{
-	config(
-		materialized='incremental',
-		alias='EXP_G20_SWP_VALUATIONSOUT_1',
-		schema='',
-		pre_hook ="",
-		post_hook ="",
-		incremental_strategy='append'
-	)
+    config(
+        materialized='table',   -- or 'incremental' if you want incremental
+        alias='EXP_G20_SWP_VALUATIONSOUT_FINAL',
+        schema=''
+    )
 }}
-Select
-	'LEI_' || LPAD('abc', 5, '0') as O_LEGAL_ENTITY_IDENTIFIER,
-    LENGTH(PARTY_CODE_DISPLAY) as CODE_DISPLAY,
-    PARTY_CODE_PREFIX_ALT_DISPLAY,
-    UTI,
-    o_UPI,
-    NPV,
-    LENGTH(CURRENCY_CODE) as CURRENCY_CODE,
-    VALUATION_METHOD,
-    VALUATION_TIMESTAMP,
-    ACTION_TYPE,
-    Proprietary_UTI_indicator
-from
 
-	EXP_G20_SWP_VALUATIONSOut
+WITH base_data AS (
+    SELECT
+        'LEI_' || LPAD('abc', 5, '0') AS O_LEGAL_ENTITY_IDENTIFIER,
+        LENGTH(PARTY_CODE_DISPLAY) AS CODE_DISPLAY,
+        PARTY_CODE_PREFIX_ALT_DISPLAY,
+        UTI,
+        o_UPI,
+        NPV,
+        LENGTH(CURRENCY_CODE) AS CURRENCY_CODE,
+        VALUATION_METHOD,
+        VALUATION_TIMESTAMP,
+        ACTION_TYPE,
+        Proprietary_UTI_indicator
+    FROM EXP_G20_SWP_VALUATIONSOut
+),
 
+summary AS (
+    SELECT
+        O_LEGAL_ENTITY_IDENTIFIER,
+        COUNT(*) AS TOTAL_RECORDS,
+        SUM(NPV) AS TOTAL_NPV,
+        MAX(VALUATION_TIMESTAMP) AS LAST_VALUATION_TS
+    FROM base_data
+    GROUP BY O_LEGAL_ENTITY_IDENTIFIER
+)
+
+-- 👇 Choose the final output
+SELECT * FROM summary;
