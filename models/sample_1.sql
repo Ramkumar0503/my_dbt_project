fd13a9eb61cd7ad701d3bd2f5b4e4d2545b5a441
Ramkumar0503 <ramkumarayyadurai05@gmail.com>
2025-09-25 12:20:18 +0530

    Update sample_1.sql

--- models/sample_1.sql
+++ models/sample_1.sql
@@ -1,36 +1,35 @@
 {{ config(
     materialized='incremental',
-    unique_key='valuation_id',
+    unique_key='_fivetran_id',
     incremental_strategy='merge',
     on_schema_change='sync_all_columns'
 ) }}
 
 with demo as (
     select 
-        valuation_id,
-        amount as demo_amount,
-        valuation_date as demo_date
+        _fivetran_id,
+        NPV as demo_amount,
+        VALUATION_TIMESTAMP as demo_date
     from {{ source('snowflake_source_demo_schema', 'EXP_G20_SWP_VALUATIONSOUT') }}
 ),
 
 abc as (
     select 
-        valuation_id,
-        amount as abc_amount,
-        valuation_date as abc_date
+        _fivetran_id,
+        null as abc_amount,  -- No NPV in this table, placeholder
+        START_TIME as abc_date
     from {{ source('snowflake_source_2_abc', 'EXP_G20_SWP_VALUATIONSOUT_301') }}
 )
 
 select 
-    d.valuation_id,
+    d._fivetran_id,
     d.demo_amount,
     a.abc_amount,
-    -- expression logic: difference between amounts
-    (a.abc_amount - d.demo_amount) as amount_diff,
+    (coalesce(a.abc_amount, 0) - coalesce(d.demo_amount, 0)) as amount_diff,
     coalesce(a.abc_date, d.demo_date) as final_date
 from demo d
 left join abc a 
-    on d.valuation_id = a.valuation_id
+    on d._fivetran_id = a._fivetran_id
 
 {% if is_incremental() %}
   where coalesce(a.abc_date, d.demo_date) > (select max(final_date) from {{ this }})
