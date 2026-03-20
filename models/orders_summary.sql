7b815598f81d22a4c865db3c69e242a262c6b03b
Ramkumar <79976137+PunyaKeerthi1@users.noreply.github.com>
2026-03-20 19:24:34 +0530

    Auto-update: DBT models and configurations

--- models/orders_summary.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    customer_id,
-    SUM(amount) AS total_amount
-FROM {{ ref('stg_orders') }}
-GROUP BY customer_id
--- models/stg_orders.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    order_id,
-    customer_id,
-    amount,
-    order_date,
-    status
-FROM {{ source('dbo', 'orders') }}
